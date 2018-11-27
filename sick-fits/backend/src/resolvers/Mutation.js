39fb80713118834beeb7906087fa7df64d4c53be
kevinegstorf <kevin.egstorf@gmail.com>
2018-11-27 20:05:24 +0100

    done till vid 17

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
+	createItem(parent, args, ctx, info) {
+		//  TODO: check if they are logged in
 
-module.exports = mutations;
+		const item = ctx.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		);
+		return item;
+	}
+};
+
+module.exports = Mutations;
